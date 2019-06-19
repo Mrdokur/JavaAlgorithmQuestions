4266ebce1a80a8aebc14065d7f80b84a5a500d7a
mrdkr35@gmail.com <mrdkr35@gmail.com>
2019-06-19 11:00:54 -0400

    First or Last vowel class is added

---
+++ src/JavaAlgorithm/FirstOrLastVowel.java
@@ -0,0 +1,36 @@
+package JavaAlgorithm;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class FirstOrLastVowel {
+	
+	
+	// Return "yes" if the first letter or last letter of the string is a vowel [ aeiouAEIOU ]. 
+	// Return "no" if the string does not start or end with a vowel.
+	
+
+	public static void main(String[] args) {
+		
+		String str = "This is a string!";
+		System.out.println(go(str));
+	
+		
+
+	}
+
+	public static String go(String str) {
+		String vowels = "aeiouAEIOU";
+		
+		int pos1 = vowels.indexOf(str.substring(0, 1));
+		int pos2 = vowels.indexOf(str.substring(str.length()-1));
+		
+		if(pos1>-1 || pos2>-1)
+			return "yes";
+		else
+			return "no";
+	}
+	
+	
+}
