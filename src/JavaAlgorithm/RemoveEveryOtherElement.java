c362a456e7305234092dbc0bcba1b2747b238c09
Ahmet Dokur <mrdkr35@gmail.com>
2019-06-24 13:33:50 -0400

    Updates on 'Remove every other element' class

--- src/JavaAlgorithm/RemoveEveryOtherElement.java
+++ src/JavaAlgorithm/RemoveEveryOtherElement.java
@@ -1,10 +1,32 @@
 package JavaAlgorithm;
 
+import java.util.ArrayList;
+
 public class RemoveEveryOtherElement {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		// This method will remove every other element from an array list
+		// and print the remaining items as a String
+		
+		
+		
+		
 
 	}
+	
+	
+	
+	public static String removeElements(ArrayList list) {
+		
+		
+		
+		
+		
+		
+		return "";
+	}
+	
+	
 
 }
