d4d3a097d529c7d242b027f7af7c0dd87075eb4b
Ahmet Dokur <mrdkr35@gmail.com>
2019-06-24 21:25:41 -0400

    some updates on 'removeEveryOtherElement' class made.

--- src/JavaAlgorithm/RemoveEveryOtherElement.java
+++ src/JavaAlgorithm/RemoveEveryOtherElement.java
@@ -17,7 +17,16 @@ public class RemoveEveryOtherElement {
 	
 	
 	
-	public static String removeElements(ArrayList list) {
+	public static String removeElements(ArrayList<String> list) {
+		
+				
+		// lets create a copy of the array list first
+		
+		ArrayList<String> copyOfList = new ArrayList<String>();
+		
+		for (int i = 0; i < list.size(); i++) {
+			copyOfList.add(list.get(i));
+		}
 		
 		
 		
